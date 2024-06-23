da968b9b9f179b428894c355cb0d001cfbe61fd4
Sam John <maniextra22@gmail.com>
2024-06-23 21:10:46 +0500

    Update loginPage.dart

--- lib/Auth/loginPage.dart
+++ lib/Auth/loginPage.dart
@@ -134,7 +134,7 @@ class _LoginPageState extends State<LoginPage> {
                     child: Text(
                       'Sign Up',
                       style: TextStyle(
-                        color: Colors.brown,
+                        color: Colors.blue,
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
                       ),
